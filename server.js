a6dd6ff2b5990f4a221f9e478f100349c60b2136
Connor Lawrence <connor.lawrence96@gmail.com>
2021-11-27 19:51:55 -0700

    housekeeping

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
-const PORT = process.env.PORT || 3002;
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
-app.use(express.json());
-
-
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -43,31 +35,6 @@ function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id)[0];
   return result;
 }
-function createNewAnimal(body, animalsArray) {
-  const animal = body;
-  animalsArray.push(animal);
-  fs.writeFileSync(
-    path.join(__dirname, './data/animals.json'),
-    JSON.stringify({ animals: animalsArray }, null, 2)
-  );
-  return animal;
-}
-
-function validateAnimal(animal) {
-  if (!animal.name || typeof animal.name !== 'string') {
-    return false;
-  }
-  if (!animal.species || typeof animal.species !== 'string') {
-    return false;
-  }
-  if (!animal.diet || typeof animal.diet !== 'string') {
-    return false;
-  }
-  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-    return false;
-  }
-  return true;
-}
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -85,19 +52,10 @@ app.get('/api/animals/:id', (req, res) => {
     res.send(404);
   }
 });
-app.post('/api/animals', (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
-  } else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-  }
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
 
 
 app.listen(PORT, () => {
